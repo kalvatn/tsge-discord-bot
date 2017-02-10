f680a228cafcd0be78180a6e688817242faec9b9
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2017-02-10 15:44:12 +0100

    added some more lastfm/xkcd features, cleanup

--- bot.js
+++ bot.js
@@ -3,240 +3,218 @@
 var Discord = require('discord.io');
 var http = require('http');
 var https = require('https');
+var request = require('request-promise');
 
 
 const DISCORD_BOT_TOKEN = '';
 
 const LAST_FM_API_KEY = '';
-const LASTFM_BASE_API_URL = 'http://ws.audioscrobbler.com/2.0/?api_key=' + LAST_FM_API_KEY + '&limit=2&format=json'
+const LASTFM_BASE_API_URL = 'http://ws.audioscrobbler.com/2.0/?api_key=' + LAST_FM_API_KEY + '&format=json'
+
+const LASTFM_METHODS = {
+  'nowplaying' : 'user.getRecentTracks',
+  'topalbums'  : 'user.getTopAlbums',
+  'toptracks'  : 'user.getTopTracks'
+}
 
 const COMMAND_PREFIXES = [ '!', '?' ];
 const DEBUG = false;
 
-console.log('creating discord client');
+
+console.info('creating discord client');
 var bot = new Discord.Client({
   token: DISCORD_BOT_TOKEN,
   autorun: !DEBUG
 });
 
 bot.on('ready', function() {
-  console.log('logged in as %s - %s\n', bot.username, bot.id);
+  console.info(`logged in as ${bot.id} (${bot.username})`);
 });
 
-bot.on('message', function(user, userID, channelID, message, e) {
-  try {
-    processMessage(user, userID, channelID, message);
-  } catch(error) {
-    console.log('error : ', error);
-  }
+bot.on('message', function(username, user_id, channel_id, message, e) {
+  handle_message(username, user_id, channel_id, message);
 });
-bot.on('disconnect', function(errMsg, code) {
-  console.log('disconnected : ', code, ' ', errMsg);
+
+bot.on('disconnect', function(error, error_code) {
+  console.warn(`disconnected due to ${error} (${error_code}), reconnecting..`);
   bot.connect();
 });
 
-// bot.connect();
-
 if (DEBUG) {
-  // processMessage('kalvatn', 1, 1, '!ping', null);
-  // processMessage('kalvatn', 1, 1, '!xkcd', null);
-  // processMessage('kalvatn', 1, 1, '!lastfm chauney', null);
-  // processMessage('kalvatn', 1, 1, '!lastfm', null);
-  // processMessage('kalvatn', 1, 1, '!help chauney', null);
-  // processMessage('kalvatn', 1, 1, '?help', null);
-  // processMessage('kalvatn', 1, 1, '?commands', null);
-  // processMessage('kalvatn', 1, 1, '?lol', null);
-  // processMessage('kalvatn', 1, 1, '#lol', null);
-  processMessage('kalvatn', 1, 1, '!lastfm chauney', null);
-
-  // processMessage('kalvatn', 1, 1, '!xkcd', null);
+  handle_message('kalvatn', 1, 1, '!lastfm chauney', null);
+  handle_message('kalvatn', 1, 1, '!xkcd 1', null);
+  handle_message('kalvatn', 1, 1, '!xkcd', null);
+  handle_message('kalvatn', 1, 1, '!tts hello world', null);
+  handle_message('kalvatn', 1, 1, '!lastfm chauney nowplaying', null);
+  handle_message('kalvatn', 1, 1, '!lastfm chauney topalbums', null);
+  handle_message('kalvatn', 1, 1, '!lastfm chauney toptracks', null);
 }
 
-function processMessage(user, userID, channelID, message) {
-  if (userID == bot.id) {
+function handle_message(user, user_id, channel_id, message) {
+  if (user_id == bot.id) {
     return;
   }
 
-  var tokens = message.split(' ');
-  if (!tokens && tokens.length <= 0) {
-    return;
-  }
-  var isCommand = false;
-  for (var i=0; i < COMMAND_PREFIXES.length; i++) {
-    if (tokens[0].startsWith(COMMAND_PREFIXES[i])) {
-      isCommand = true;
-      break;
-    }
-  }
-
-  if (!isCommand) {
+  var messageSplit = message.split(' ');
+  if (!messageSplit && messageSplit.length <= 0) {
     return;
   }
 
-  console.log('incoming message from user %s (%d) channel %d : "%s"', user, userID, channelID, message);
-  var command = tokens[0].substr(1, tokens[0].length);
-  var args = tokens.slice(1, tokens.length);
+  if (COMMAND_PREFIXES.indexOf(messageSplit[0].charAt(0)) < 0) {
+    return;
+  }
+
+  console.info(`received command from user ${user_id} (${user}) in channel ${channel_id} : '${message}'`);
+  var command = messageSplit[0].substr(1, messageSplit[0].length);
+  var args = messageSplit.slice(1, messageSplit.length);
   switch (command) {
-    // case 'tts':
-    //   // sendTTSMessage(channelID, args.join(' '));
-    //   break;
-    case 'ping':
-    case 'alive':
-      sendTextMessage(channelID, 'pong');
+    case 'tts':
+      send_text_message(channel_id, args.join(' '), true);
       break;
-    // case 'list':
-    //   console.log(args);
-    //   if (args.length >= 1) {
-    //     var limit = args[0];
-    //     listMessages(channelID, limit);
-    //   }
-    //   break;
     case 'lastfm':
     case 'np':
-      if (args.length == 1) {
-        var lastfmUsername = args[0];
-        lastfmNowPlaying(lastfmUsername, channelID);
-      } else {
-        sendHelpMessage(userID);
-      }
+      lastfm(args, (response) => {
+        response.forEach((r) => {
+          send_text_message(channel_id, r);
+        });
+      }, (error) => {
+        send_text_message(channel_id, error);
+      });
       break;
     case 'xkcd':
-      xkcdLatest(channelID);
+      xkcd(args, (response) => {
+        send_text_message(channel_id, response);
+      }, (error) => {
+        send_text_message(channel_id, error);
+      });
       break;
     case 'help':
     case 'commands':
     default:
-      sendHelpMessage(userID);
+      send_help_message(user_id);
       break;
   }
 }
 
-function sendHelpMessage(discordID) {
+function send_help_message(discord_id) {
   var help = 'available commands:\n';
-  help += '\t\t!lastfm <username>\n';
-  help += '\t\t!xkcd\n';
-  sendTextMessage(discordID, help);
+  help += '\t\t!lastfm <username> [nowplaying|topalbums]\n';
+  help += '\t\t!xkcd [number]\n';
+  send_text_message(discord_id, help);
 }
 
-function sendTextMessage(discordID, message) {
+function send_text_message(discord_id, message, text_to_speech) {
   if (DEBUG) {
-    console.log('DEBUG MODE : would have sent message "%s" to id : %d', message, discordID);
+    console.log(`DEBUG MODE : would have sent ${(text_to_speech) ? 'text-to-speech' : 'text'} message '${message}'`);
     return;
   }
   bot.sendMessage({
-    to: discordID, // user/channel
-    message: message
-  });
-}
-
-
-function listMessages(discordID, limit) {
-  bot.getMessages({
-    channelID: discordID,
-    limit: limit
-  }, function(error, messages) {
-    if (error) {
-      console.log('error : ', error);
-    } else {
-      var filtered = messages.slice(0, limit);
-      filtered.forEach(m => {
-        console.log('message %d : "%s"', m.id, m.content);
-        sendTextMessage(discordID, m.id + ' ' + m.content);
-      });
-      // deleteMessage(filtered[0].id);
-    }
-  });
-
-}
-
-function deleteMessage(discordID, messageID) {
-  sendTextMessage(discordID, 'deleting message ' + messageID + ' from channel ' + discordID);
-  bot.deleteMessage({
-    channelID: discordID,
-    messageID: messageID
-  }, function(error, response) {
-    console.log('error : ', error, ' response : ', response);
-  });
-
-}
-
-function sendTTSMessage(discordID, message) {
-
-  if (DEBUG) {
-    console.log('DEBUG MODE : would have sent TTS message "%s" to id : %d', message, discordID);
-    return;
-  }
-  bot.sendMessage({
-    to: discordID,
+    to: discord_id,
     message: message,
-    tts: true
-  });
-
-}
-
-function httpGET(url, callback) {
-  http.get(url, function(response) {
-    processHTTPResponse(response, callback);
-  }).on('error', function(error) {
-    console.log('error: ', error);
-  });
-}
-
-function httpsGET(url, callback) {
-  https.get(url, function(response) {
-    processHTTPResponse(response, callback);
-  }).on('error', function(error) {
-    console.log('error: ', error);
-  });
-}
-
-function processHTTPResponse(response, callback) {
-  var responseBody = '';
-  response.on('data', function(chunk) {
-    responseBody += chunk;
-  });
-
-  response.on('end', function() {
-    callback(responseBody);
-  });
-}
-
-function readUrl(url, callback) {
-  if (url.startsWith('https')) {
-    httpsGET(url, callback);
-  } else {
-    httpGET(url, callback);
-  }
-}
-function readJSON(url, callback) {
-  readUrl(url, function(data) {
-    var jsonData = JSON.parse(data);
-    callback(jsonData);
-  });
-}
-
-function xkcdLatest(channelID) {
-  var url = 'https://xkcd.com/info.0.json';
-  readJSON(url, function(data) {
-    var message = data.safe_title + ' - ' + data.img + ' - ' + data.alt;
-    sendTextMessage(channelID, message);
-  });
-
-}
-
-function lastfmNowPlaying(username, channelID) {
-  var url = LASTFM_BASE_API_URL + '&user=' + username + '&method=user.getRecentTracks';
-  readJSON(url, function(data) {
-    if (data.recenttracks) {
-      var last = data.recenttracks.track[0]
-      if (last) {
-        var response = last.artist['#text'] + ' - ' + last.album['#text'] + ' - ' + last.name;
-        sendTextMessage(channelID, response);
-      } else {
-        sendTextMessage(channelID, 'no last tracks for user ' + username);
-      }
-    } else {
-      sendTextMessage(channelID, 'invalid user');
+    tts: (text_to_speech) ? true : false
+  }, (error, response) => {
+    if (error) {
+      console.error('error : ', error);
     }
   });
 }
+
+function get_json(url, callback, error_callback) {
+  request(url)
+    .then((data) => {
+      callback(JSON.parse(data));
+    })
+    .catch((error) => {
+      error_callback(error);
+    });
+}
+
+function lastfm(args, callback, error_callback) {
+  if (1 < args.length > 2) {
+    error_callback('usage: !lastfm <username> [nowplaying|topalbums|toptracks]');
+    return;
+  }
+  var username = args[0];
+  if (args[1] && !LASTFM_METHODS[args[1]]) {
+    error_callback('usage: !lastfm <username> [nowplaying|topalbums|toptracks]');
+    return;
+  }
+  var method = (args[1]) ? args[1] : 'nowplaying';
+  var url = `${LASTFM_BASE_API_URL}&user=${username}&method=${LASTFM_METHODS[method]}&limit=5`;
+
+  get_json(url, (data) => {
+    switch (method) {
+      case 'nowplaying':
+        if (data.recenttracks) {
+          var last_played = data.recenttracks.track[0];
+          if (last_played) {
+            var artist = last_played.artist['#text'];
+            var album = last_played.album['#text'];
+            var title = last_played.name;
+            callback([`${artist} - ${album} - ${title}`]);
+          } else {
+            error_callback(`no last_played tracks for user ${username}`);
+          }
+        } else {
+          error_callback(`could not fetch last played for user ${username}`);
+        }
+        break;
+      case 'topalbums':
+        if (data.topalbums) {
+          var albums = data.topalbums.album.slice(0, 10);
+
+          var reply = [];
+
+          albums.forEach((album) => {
+            var artist = album.artist.name;
+            var name = album.name;
+            var playcount = album.playcount;
+
+            reply.push(`${artist} - ${name}`);
+          });
+          callback(reply);
+        } else {
+          error_callback('could not fetch top albums for user ${username}');
+        }
+        break;
+      case 'toptracks':
+        if (data.toptracks) {
+          var tracks = data.toptracks.track.slice(0, 10);
+          var reply = [];
+
+          tracks.forEach((track) => {
+            var artist = track.artist.name;
+            var name = track.name;
+            var playcount = track.playcount;
+
+            reply.push(`${artist} - ${name}`);
+          });
+          callback(reply);
+        } else {
+          error_callback(`could not fetch top tracks for user ${username}`);
+        }
+        break;
+      default:
+        error_callback(`unsupported lastfm api method '${method}', available choices are : nowplaying, topalbums, toptracks`);
+        break;
+    }
+  }, (error) => {
+    error_callback(error);
+  });
+}
+
+function xkcd(args, callback, error_callback) {
+  var url = 'https://xkcd.com/info.0.json';
+  if (args.length == 1 && args[0] > 0) {
+    url = `https://xkcd.com/${args[0]}/info.0.json`;
+  }
+  get_json(url, (data) => {
+    var title = data.safe_title;
+    var img = data.img;
+    var alt_text = data.alt;
+    callback(`${title} : ${img} (${alt_text})`);
+  }, (error) => {
+    error_callback(error);
+  });
+}
+
