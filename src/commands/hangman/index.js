905020ba7e357c6a2722f47667af1d5259a1d4e6
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2017-03-08 15:50:09 +0100

    rewrite as class

--- src/commands/hangman/index.js
+++ src/commands/hangman/index.js
@@ -4,6 +4,8 @@ import rp from 'request-promise';
 import logger from '../../util/logging';
 import string from '../../util/string';
 
+const GRAPHIC_PARTS = [ 'O', '\/', '\\', '|', '\/', '\\' ];
+const LIVES = GRAPHIC_PARTS.length;
 const GRAPHIC_INITIAL = [
   '  +-----+ ',
   '  |     | ',
@@ -14,31 +16,132 @@ const GRAPHIC_INITIAL = [
   '__+__     '
 ].join('\n');
 
+const GRAPHIC_GAME_OVER = [
+  '  +-----+ ',
+  '  |     | ',
+  '  |     O         game over!',
+  '  |    \/ \\',
+  '  |     | ',
+  '  |    \/ \\',
+  '__+__     '
+].join('\n');
+
 const GRAPHIC_SUCCESS = [
   '  +-----+         ',
   '  |     |         ',
-  '  |               yes! :D :D :D',
+  '  |               correct!',
   '  |        \\O\/   ',
+  '  |         v      ',
   '  |         |     ',
   '__+__      \/ \\   '
 ].join('\n');
 
+class Game {
+  constructor(word) {
+    this.word = word;
+    this.attempts = 0;
+    this.word_masked = this.word.replace(/\w/g, '_');
+    this.solved = false;
+    this.wrong_guesses = new Set();
+    this.correct_letters = new Set();
+    this.graphic = GRAPHIC_INITIAL;
+    this.update_graphic();
+  }
 
-const GRAPHIC_PARTS = [ 'O', '\/', '\\', '|', '\/', '\\' ];
+  printable() {
+    let output = [];
+    output.push(this.graphic);
+    output.push(`tried [ ${[...this.wrong_guesses].join(', ')} ] tries left : ${LIVES - this.attempts}`);
+    output.push([...this.word_masked].join(' '));
+    if (this.attempts >= LIVES) {
+      output.push([...this.word].join(' '));
+    }
+    return output.join('\n');
+  }
 
-const LIVES = GRAPHIC_PARTS.length;
-let word;
-let attempts;
-let wrong_letters;
-let wrong_full_guesses;
-let correct_letters;
-let in_progress = false;
+  abort() {
+    this.attempts = LIVES;
+    this.update_graphic();
+  }
+
+  guess_and_print(word_or_letter) {
+    if (this.solved || this.attempts >= LIVES) {
+      throw 'game finished';
+    }
+    this.guess(word_or_letter);
+    logger.debug(this.printable());
+  }
+
+  guess(word_or_letter) {
+    if (this.is_correct(word_or_letter)) {
+      if (this.word_masked === this.word) {
+        this.solved = true;
+      }
+    }
+    this.update_graphic();
+  }
+
+  is_correct(word_or_letter) {
+    if (this.word.indexOf(word_or_letter) > -1) {
+      this.update_correct_letters([...word_or_letter]);
+      return true;
+    }
+
+    if (!this.wrong_guesses.has(word_or_letter)) {
+      this.wrong_guesses.add(word_or_letter);
+      this.attempts += 1;
+      return false;
+    }
+    return true;
+  }
+
+  update_correct_letters(letters) {
+    for (let l of letters) {
+      this.correct_letters.add(l);
+    }
+    this.update_mask();
+  }
+
+  update_mask() {
+    let masked = [];
+    for (let i=0; i < this.word.length; i++) {
+      let letter = this.word.charAt(i);
+      if (this.correct_letters.has(letter)) {
+        masked[i] = letter;
+      } else {
+        masked[i] = '_';
+      }
+    }
+    this.word_masked = masked.join('');
+  }
+
+  update_graphic() {
+    if (this.solved) {
+      this.graphic = GRAPHIC_SUCCESS;
+      return;
+    }
+    if (this.attempts >= LIVES) {
+      this.graphic = GRAPHIC_GAME_OVER;
+      return;
+    }
+    let graphic = GRAPHIC_INITIAL;
+    for (let i=0; i <= this.attempts-1; i++) {
+      graphic = graphic.replace('#', GRAPHIC_PARTS[i]);
+    }
+    while (graphic.indexOf('#') > -1) {
+      graphic = graphic.replace('#', ' ');
+    }
+    this.graphic = graphic;
+  }
+}
+
+let game;
 
 function hangman(args) {
   return new Promise((resolve, reject) => {
     let command = args[0];
     if (!command) {
-      if (!in_progress) {
+      if (!game) {
         command = 'new';
       } else {
         return reject(usage);
@@ -48,13 +151,14 @@ function hangman(args) {
     let output = [];
     switch (command) {
       case 'new':
-        if (in_progress) {
-          output.push(show_solution());
+        if (game) {
+          game.abort();
+          output.push(game.printable());
         }
         random_word()
           .then(result => {
-            new_game(result.toLowerCase());
-            output.push(build_output());
+            game = Game(result.toLowerCase());
+            output.push(game.printable());
             return resolve(string.markdown(output.join('\n')));
           })
           .catch(error => {
@@ -62,9 +166,9 @@ function hangman(args) {
           });
         break;
       default:
-        if (in_progress) {
-          guess(command.toLowerCase());
-          output.push(build_output());
+        if (game) {
+          game.guess(command.toLowerCase());
+          output.push(game.printable());
         } else {
           output.push('you need to start a new game before guessing');
         }
@@ -73,99 +177,8 @@ function hangman(args) {
   });
 }
 
-function new_game(new_word) {
-  word = new_word;
-  attempts = 0;
-  wrong_letters = new Set();
-  wrong_full_guesses = new Set();
-  correct_letters = new Set();
-  in_progress = true;
-  logger.debug(`new game started, word : ${word}`);
-}
-
 function random_word() {
   return rp('http://www.setgetgo.com/randomword/get.php');
-}
-
-function guess(word_or_letter) {
-  if (!in_progress) {
-    return;
-  }
-  if (word_or_letter.length == 1) {
-    if (word.indexOf(word_or_letter) >= 0) {
-      correct_letters.add(word_or_letter);
-    } else {
-      if (!wrong_letters.has(word_or_letter)) {
-        attempts += 1;
-        wrong_letters.add(word_or_letter);
-      }
-    }
-  } else {
-    if (word_or_letter === word) {
-      [...word_or_letter].forEach(l => {
-        correct_letters.add(l);
-      });
-    } else {
-      if (!wrong_full_guesses.has(word_or_letter)) {
-        attempts += 1;
-        wrong_full_guesses.add(word_or_letter);
-      }
-    }
-  }
-}
-
-
-function build_output() {
-  let masked = mask_word();
-  let output = [];
-  if (masked === word) {
-    in_progress = false;
-    output.push(GRAPHIC_SUCCESS);
-    output.push(`\n${[...word].join(' ')}`);
-  } else {
-    output.push(update_hangman());
-    if (wrong_letters.size > 0) {
-      output.push(`wrong_letters : [ ${[...wrong_letters].sort().join(' ')} ]`);
-    }
-    if (wrong_full_guesses.size > 0) {
-      output.push(`wrong_letters wrong_full_guesses : [ ${[...wrong_full_guesses].join(', ')} ]`);
-    }
-    if (attempts === LIVES) {
-      output.push(`\n${[...word].join(' ')}`);
-      in_progress = false;
-    } else {
-      output.push(`\n${[...masked].join(' ')}`);
-    }
-  }
-  return output.join('\n');
-}
-
-function show_solution() {
-  return `solution : ${[...word].join(' ')}`;
-}
-
-function update_hangman() {
-  let graphic = GRAPHIC_INITIAL;
-  for (let i=0; i <= attempts-1; i++) {
-    graphic = graphic.replace('#', GRAPHIC_PARTS[i]);
-  }
-  while (graphic.indexOf('#') > -1) {
-    graphic = graphic.replace('#', ' ');
-  }
-  return graphic;
-}
-
-function mask_word() {
-  let masked = [];
-  for (let i=0; i < word.length; i++) {
-    let letter = word.charAt(i);
-    if (correct_letters.has(letter)) {
-      masked[i] = letter;
-    } else {
-      masked[i] = '_';
-    }
-  }
-  return masked.join('');
 }
 
 export default hangman;
@@ -176,3 +189,10 @@ export const name = 'hangman';
 export const delete_command_message = true;
 export const edit_replies = true;
 export const usage = '!hm <new|letter|wordguess>';
+
+
+game = new Game('hangman');
+logger.debug(game.printable());
+game.abort();
+game.guess_and_print('a');
+logger.debug(game.printable());
